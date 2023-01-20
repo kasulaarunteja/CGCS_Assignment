3dda26059f24fd602047244964bb46b14cbe9b0a
kasulaarunteja <kasulaarunteja@gmail.com>
2023-01-20 21:39:08 +0530

    characters done

--- src/Components/Characters.jsx
+++ src/Components/Characters.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
-import '../App.css';
-
+import "../App.css";
 
 const Characters = () => {
   const [hero, setHero] = useState([]);
+  const [loding, setLoding] = useState(true);
+
 
   const getdata = () => {
     axios
@@ -13,6 +14,7 @@ const Characters = () => {
         "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=a89922fc3dff570a0314a981b7c40fe8&hash=9a28b6c94c59b070a4b50200786f39b2"
       )
       .then((res) => {
+        setLoding(false);
         setHero(res.data.data.results);
         console.log(res.data.data.results);
       });
@@ -22,49 +24,49 @@ const Characters = () => {
     getdata();
   }, []);
 
-  return (
+  return loding ? (  "loding...."
+  ) : (
     <>
       <div className="b">
         <h1>Characters </h1>
       </div>
-      {hero.map((e) => {
-        return (
-          // <div className="maindiv">
-          //   <div className="card" key={e.id}>
-          //     <img
-          //       src={`${e.thumbnail.path}.${e.thumbnail.extension}`}
-          //       alt="Product Name"
-          //     />
-          //     <div className="container">
-          //       <h4>
-          //         <b>{e.name}</b>
-          //       </h4>
-          //       <p>{e.description}</p>
-          //     </div>
-          //     <button>Add to Cart</button>
-          //   </div>
-          // </div>
-          <div className="maindiv">
-                 <div className="box">
+      <div className="main">
+        {hero.map((e) => {
+          return (
+            // <div className="maindiv">
+            //   <div className="card" key={e.id}>
+            //     <img
+            //       src={`${e.thumbnail.path}.${e.thumbnail.extension}`}
+            //       alt="Product Name"
+            //     />
+            //     <div className="container">
+            //       <h4>
+            //         <b>{e.name}</b>
+            //       </h4>
+            //       <p>{e.description}</p>
+            //     </div>
+            //     <button>Add to Cart</button>
+            //   </div>
+            // </div>
+
             <div className="box1">
-              <div className="img">
+              <div className="imgdiv">
                 <img
                   src={`${e.thumbnail.path}.${e.thumbnail.extension}`}
-                 
                   alt=""
                 />
               </div>
+              <div className="titelcards">
+                <h4>{e.name}</h4>
+                <p>Action figures from the pop culture landscape</p>
+              </div>
               <div>
-                <p>{e.name}</p>
-                <h4>{e.description}</h4>
+                <button className="btn">Add to cart</button>
               </div>
             </div>
-          </div>
-          </div>
-
-          
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
